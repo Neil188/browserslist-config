3286b4a304dd8826e9fc6450a417c6276425c0e7
Neil188 <19958604+Neil188@users.noreply.github.com>
2018-09-12 18:30:36 +0100

    test: add tests

--- util/test.js
+++ util/test.js
@@ -1,11 +1,23 @@
+const assert = require('assert');
 const browserslist = require('browserslist');
 const rules = require('../index.js');
 
+const MIN_RULES = 1;
+const MIN_BROWSERS = 1;
+const MIN_COVERAGE = 70;
+
 console.log('Base rules:\n',rules,'\n',);
+assert(rules.length >= MIN_RULES,
+    `Min. allowed rules ${MIN_RULES}, but ${rules.length} loaded.`);
 
 const browsers = browserslist(rules);
+const coverage = browserslist.coverage( browsers );
+assert(browsers.length >= MIN_BROWSERS,
+    `Min. supported browsers ${MIN_BROWSERS}, but ${browsers.length} covered.`);
+assert(coverage >= MIN_COVERAGE,
+    `Coverage should be at least ${MIN_COVERAGE}%, but calculated coverage is ${coverage}%`);
 
 console.log('Browsers covered:\n', browsers.join('\n'));
 console.log('\n');
-console.log('Browser coverage: ', browserslist.coverage( browsers ));
+console.log('Browser coverage: ', coverage);
 console.log('\n');
